51659e3214a6d9dd462ceab15c8f283301cf9cbc
ewurman <ewurman@gmail.com>
2018-09-25 23:03:59 -0400

    Main now compiles for parser

--- main.cpp
+++ main.cpp
@@ -9,25 +9,22 @@
 #include <fstream>
 #include "pbil.cpp"
 #include "ga.cpp"
-
+#include <sstream>
 
 using namespace std;
+int** parse(char* filename);
 
-
-main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     char* filename = argv[1];
     int** clauses = parse(filename);
 
 
-    if (argv[8] == 'g') {
+    if (*argv[8] == 'g') {
         ga(argv);
     }
     else {
-        pbil(argv);
+        pbil(0);
     }
-    
-    
-    
     return 0;
 }
 
@@ -41,27 +38,33 @@ int** parse(char* filename){
     int numClauses = 0;
     bool initialized = false;
     int clauseNum = 0;
+    string line;
     while (getline(infile, line)){
-        if (line[0] = 'c'){
+        if (line[0] == 'c'){
             continue; //lines that start with c are comments
         }
-        if (line[0] == 'p'){
-            char p;
-            char* cnf;
-            line >> p >> cnf >> numVariables >> numClauses;
+        if (line[0] == 'p'){ 
+            // then the begining is "p cnf "
+            int pcnf = line.find("p cnf ");
+            line = line.substr(pcnf);
+            //line >> p >> cnf >> numVariables >> numClauses;
+            int space = line.find(" ");
+            numVariables = stoi( line.substr(0,space));
+            numClauses = stoi( line.substr(space));
+            
             clauses = new int*[numClauses];
             initialized = true;
         }
         else if (initialized){ 
             // grab each index from line, subtract by 1 (they are 1-based)
             // then add that index to the end of the clause array.
-            int varIndex = (int) line.substr(0,str.find(" ")) - 1;
+            int varIndex = stoi( line.substr(0,line.find(" "))) - 1;
             int clauseIndex = 0; 
             while (varIndex != -1) { //0 ends the line and we subtract 1
                 clauses[clauseNum][clauseIndex] = varIndex;
                 clauseIndex++;
-                line = line.substr(str.find(" ");
-                varIndex = (int) line.substr(0,str.find(" ")) - 1;
+                line = line.substr(line.find(" "));
+                varIndex = stoi( line.substr(0,line.find(" "))) - 1;
             }
         }
         clauseNum++;
