306d10e1039987196a9d1bd707ce29f22341b9cf
ewurman <ewurman@gmail.com>
2018-09-25 22:42:09 -0400

    parsing up but not tested

--- main.cpp
+++ main.cpp
@@ -6,10 +6,19 @@
 //
 
 #include <stdio.h>
-#include <ga.cpp>
-#include <pbil.cpp>
+#include <fstream>
+#include "pbil.cpp"
+#include "ga.cpp"
+
+
+using namespace std;
+
 
 main(int argc, char* argv[]) {
+    char* filename = argv[1];
+    int** clauses = parse(filename);
+
+
     if (argv[8] == 'g') {
         ga(argv);
     }
@@ -18,3 +27,48 @@ main(int argc, char* argv[]) {
     }
     return 0;
 }
+
+
+int** parse(char* filename){
+    // parse the file and return the array of each clause array
+    // of bool indices.
+    ifstream infile(filename);
+    int **clauses;
+    int numVariables = 0;
+    int numClauses = 0;
+    bool initialized = false;
+    int clauseNum = 0;
+    while (getline(infile, line)){
+        if (line[0] = 'c'){
+            continue; //lines that start with c are comments
+        }
+        if (line[0] == 'p'){
+            char p;
+            char* cnf;
+            line >> p >> cnf >> numVariables >> numClauses;
+            clauses = new int*[numClauses];
+            initialized = true;
+        }
+        else if (initialized){ 
+            // grab each index from line, subtract by 1 (they are 1-based)
+            // then add that index to the end of the clause array.
+            int varIndex = (int) line.substr(0,str.find(" ")) - 1;
+            int clauseIndex = 0; 
+            while (varIndex != -1) { //0 ends the line and we subtract 1
+                clauses[clauseNum][clauseIndex] = varIndex;
+                clauseIndex++;
+                line = line.substr(str.find(" ");
+                varIndex = (int) line.substr(0,str.find(" ")) - 1;
+            }
+        }
+        clauseNum++;
+    }
+    return clauses;
+}
+
+
+
+
+
+
+
--- pbil.cpp
+++ pbil.cpp
@@ -9,14 +9,22 @@
 
 using namespace std;
 
+//clock_t MAX_RUNTIME = 300 * CLOCKS_PER_SEC;
+int MAX_ITERATIONS = 1000;
+
 void pbil(int numBools){
     double* probabilities = (double*) malloc(sizeof(double)*numBools);
     
     clock_t start = clock();
-    
-    
+    int i = 0;
+    while (i < MAX_ITERATIONS){
+        i++;
 
 
+
+
+    }
+    
 }
 
 
