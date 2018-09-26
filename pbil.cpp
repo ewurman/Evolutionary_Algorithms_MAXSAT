f83cffe8fa33408adef73fd4fd01e9fcc37658a7
ewurman <ewurman@gmail.com>
2018-09-25 21:24:13 -0400

    pbil file

---
+++ pbil.cpp
@@ -0,0 +1,22 @@
+/* This is our PBIL MAXSAT Problem file
+ * Authors: Erik Wurman and Ian Squiers
+ *
+ */
+
+#include <vector>
+#include <ctime>
+
+
+using namespace std;
+
+void pbil(int numBools){
+    double* probabilities = (double*) malloc(sizeof(double)*numBools);
+    
+    clock_t start = clock();
+    
+    
+
+
+}
+
+
